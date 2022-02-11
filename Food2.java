f7e9e964a35d85e169af3726aaa9518b7e13e649
onpul <chmj072@gmail.com>
2022-02-11 13:01:13 +0900

    ingput 메소드 출력 구문 완료(점심시간 전 최종)

--- Food2.java
+++ Food2.java
@@ -131,29 +131,17 @@ public class Food2
 		//System.out.println(sauce.get("스윗 어니언") + "스윗 어니언 수량");
 		//--==>>100스윗 어니언 수량
 
-		//ingPut() 
+		//ingPut() // 재료 넣기 메소드
 		//{
 			Scanner sc = new Scanner(System.in);
-			String ingre = "";
 			System.out.print("어떤 종류의 수량 관리 할래? : "); // 빵 야채 소스
-			String kinds = " ";
-			kinds = sc.next();
+			String kinds = sc.next();
 			
 			System.out.print(kinds + " 종류 입력해줘 : "); // 화이트
-			ingre = sc.next(); 
+			String ingre = sc.next(); 
 			
-			/*
-			switch (kinds)
-			{
-				case "빵" : System.out.println(ingre + "의 수량은 " + bread.get(ingre) + "이야."); break;
-				case "야채" : System.out.println(ingre + "의 수량은 " + vegetable.get(ingre) + "이야."); break;
-				case "소스" : System.out.println(ingre + "의 수량은 " + sauce.get(ingre) + "이야."); break;
-				case "치즈" : System.out.println(ingre + "의 수량은 " + cheese.get(ingre) + "이야."); break;
-				case "사이드 메뉴" : System.out.println(ingre + "의 수량은 " + sidemenu.get(ingre) + "이야."); break;
-			}
-			*/
-			int tmp = 0;
-			switch (kinds)
+			int tmp = 0; // 재료 수량 기록할 임시 변수
+			switch (kinds) // 현재 재료 수량을 tmp에 담기
 			{
 				case "빵" : tmp = bread.get(ingre); break;
 				case "야채" : tmp = vegetable.get(ingre); break;
@@ -161,36 +149,13 @@ public class Food2
 				case "치즈" : tmp = cheese.get(ingre); break;
 				case "사이드 메뉴" : tmp = sidemenu.get(ingre); break;
 			}
-			System.out.printf("%s의 수량은 %d이야.%n", ingre, tmp);
-			System.out.print("얼마나 늘릴래?: ");
+			System.out.printf("%s의 현재 수량은 %d이야.%n", ingre, tmp);
+			System.out.print("얼마나 늘릴래? : ");
 			int su = sc.nextInt();
 			
 			tmp += su;
-			/*
-			if (kinds == "빵")
-			{
-				bread.put(ingre,tmp);
-			}
-			else if (kinds == "야채")
-			{
-				vegetable.put(ingre,tmp);
-				System.out.println("까꿍");
-			}
-			else if (kinds == "소스")
-			{
-				sauce.put(ingre,tmp);
-			}
-			else if (kinds == "치즈")
-			{
-				cheese.put(ingre,tmp);
-			}
-			else if (kinds == "사이드 메뉴")
-			{
-				sidemenu.put(ingre,tmp);
-			}
-			*/
-
-			switch (kinds)
+			
+			switch (kinds) // 기존 수량 + 사용자가 입력한 수량을 put하기 (덮어쓰기)
 			{
 				case "빵" : bread.put(ingre,tmp); break;
 				case "야채" : vegetable.put(ingre,tmp); break;
@@ -198,12 +163,16 @@ public class Food2
 				case "치즈" : cheese.put(ingre,tmp); break;
 				case "사이드 메뉴" : sidemenu.put(ingre,tmp); break;
 			}
-			System.out.println(ingre + "의 수량은 " + vegetable.get(ingre) + "이야.");
 
-			
-			
-
-
+			switch (kinds) // 반영 후 현재 수량 출력 
+			{
+				case "빵" : tmp = bread.get(ingre); break;
+				case "야채" : tmp = vegetable.get(ingre); break;
+				case "소스" : tmp = sauce.get(ingre); break;
+				case "치즈" : tmp = cheese.get(ingre); break;
+				case "사이드 메뉴" : tmp = sidemenu.get(ingre); break;
+			}
+			System.out.printf("%s의 증가 후 수량은 %d이야.%n", ingre, tmp);	
 		//}
 
 	}
