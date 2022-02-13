4053d85dd1b4c19a1589f43999847d8c6a2f5ddf
hongsm37 <hongsm37@hanmail.net>
2022-02-13 15:33:32 +0900

    샐러드

--- Salad.java
+++ Salad.java
@@ -6,7 +6,7 @@ class Salad extends SuperMenu
 {
 	String sCategory;     // 디폴트 메뉴 담기
 	int sPrice;           // 샐러드가격 담기;
-	static int sCount;
+	static int sCount = 1;
 
 	
 	/* 담기
@@ -24,7 +24,7 @@ class Salad extends SuperMenu
 		ccSelect();          //치즈여부선택하기();
 		vdCustom();          //야채 커스텀();
 		scCustom();          //소스 커스텀();
-		//addCount();          //개수추가메소드();
+		ig.addCount();          //개수추가메소드();
 		//addMenu();           //추가메뉴메소드();
 	}
 
